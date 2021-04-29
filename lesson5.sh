aa74aaffca6590f03dbabf35a573b2ca9a4e69ee
Cloud User <ec2-user@ip-172-31-45-3.us-east-2.compute.internal>
2021-04-29 21:07:47 +0000

    lesson5

---
+++ lesson5.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Demoggification ( or UUOC:UselessUseofcat)
+
+#dont do this
+# cat <file> | grep <phrase>
+
+# do this
+# grep <phrase> <file>
+
+#or
+# grep < <file>
+
+# use netcat to see if server is up and running on port 80
+# $ nc -z www.acloud.guru 80 >&/dev/null
+
+# $ 2> /dev/null >/dev/tcp/www.acloud.guru/80
+
